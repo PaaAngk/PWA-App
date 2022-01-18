36b6b88da1bd1b84c23d3421eec5e8dced02b5fd
Deduch <paaAngarsk@yandex.ru>
2022-01-18 13:30:24 +0800

    upd dist

--- src/components/candidate.vue
+++ src/components/candidate.vue
@@ -237,6 +237,19 @@ export default {
 
         //Редактирование кандидата
         editCandidate(){
+            if(this.lastName == '' || this.firstName== '' || this.secondName == '' || this.DOB == '' || this.placeBirth == '' || this.placeLive == ''){
+                alert('Заполните все поля');
+                return null;
+            }
+
+            //Провера даты, ввод только через точку
+            let regexp =/ ^(?:(?:31(\||.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\||.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/||\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\||\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/g ;
+            let matchAll = this.DOB.match(regexp);       
+            if(!matchAll){
+                alert("Введите корректную дату");
+                this.DOB == '';
+                return null;
+            }
             const requestOptions = {
                 method: 'PUT',
                 headers: { 'Content-Type': 'application/json' },
