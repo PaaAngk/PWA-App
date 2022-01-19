e7f3786bef0976eeb05a3da341d2c55092455cc7
Deduch <paaAngarsk@yandex.ru>
2022-01-19 18:33:34 +0800

    dist

---
+++ dist/precache-manifest.f4bc8e96c590e970917aed07d3e4e4b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60658e2ab8d8ad4da46a",
+    "url": "/css/app.51b4c05e.css"
+  },
+  {
+    "revision": "abe91756d18b7cd60871a2f47c1e8192",
+    "url": "/css/bootstrap.min.css"
+  },
+  {
+    "revision": "6acbd55c3941b4184c95e095e62b03a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60658e2ab8d8ad4da46a",
+    "url": "/js/app.a04ee4ec.js"
+  },
+  {
+    "revision": "cdc5e3a9a243107a0154",
+    "url": "/js/chunk-vendors.f74e7503.js"
+  },
+  {
+    "revision": "10bf5383f20cd7e2503f38210e25b975",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
